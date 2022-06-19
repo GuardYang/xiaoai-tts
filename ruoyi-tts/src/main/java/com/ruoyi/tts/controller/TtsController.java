531a942cac5a4f04820cb25d92d5a39e52dbf171
刘元博 <1553592282@qq.com>
2022-06-19 16:47:51 +0800

    测试

--- ruoyi-tts/src/main/java/com/ruoyi/tts/controller/TtsController.java
+++ ruoyi-tts/src/main/java/com/ruoyi/tts/controller/TtsController.java
@@ -1,9 +1,15 @@
 package com.ruoyi.tts.controller;
 
+import cn.hutool.core.lang.UUID;
 import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+import com.ruoyi.common.constant.Constants;
 import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.redis.RedisCache;
+import com.ruoyi.common.utils.ServletUtils;
 import com.ruoyi.tts.dto.AuthInfo;
 import com.ruoyi.tts.dto.LoginParam;
+import com.ruoyi.tts.dto.SayParam;
 import com.ruoyi.tts.dto.Session;
 import com.ruoyi.tts.service.TtsService;
 import lombok.extern.slf4j.Slf4j;
@@ -18,15 +24,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class TtsController {
     @Autowired
     private TtsService ttsService;
+    @Autowired
+    private RedisCache redisCache;
 
     @RequestMapping("/login")
     public AjaxResult login(@RequestBody LoginParam loginParam) {
         AuthInfo authInfo = ttsService.serviceAuth(loginParam.getUsername(), loginParam.getPassword());
         Session session = ttsService.loginMiAi(authInfo);
         JSONArray devices = ttsService.getDevice(session);
-        if (devices.size() == 0) {
+        if (devices.size() < 1) {
             return AjaxResult.error(201, "没有在线设备");
         }
-        return AjaxResult.success(devices);
+        session.setDeviceId(devices.getJSONObject(0).getString("deviceID"));
+        String token = UUID.fastUUID().toString();
+        redisCache.setCacheObject(token, session);
+        return AjaxResult.success().put(Constants.TOKEN, token);
+    }
+
+    @RequestMapping("/say")
+    public AjaxResult say(@RequestBody SayParam sayParam) {
+        String token = ServletUtils.getRequest().getHeader(Constants.TOKEN);
+        Session session = redisCache.getCacheObject(token);
+        JSONObject jsonObject = ttsService.say(session, sayParam.getText());
+        if (jsonObject.getInteger("code") != 0) {
+            return AjaxResult.error("我猜你没输入内容！");
+        }
+        return AjaxResult.success(jsonObject);
     }
 }
---
+++ ruoyi-tts/src/main/java/com/ruoyi/tts/dto/SayParam.java
@@ -0,0 +1,8 @@
+package com.ruoyi.tts.dto;
+
+import lombok.Data;
+
+@Data
+public class SayParam {
+    private String text;
+}
